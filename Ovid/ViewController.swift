b61990636f82fb05cb25bf9d30e83aa4d64ca416
choloo <choaimeloo@gmail.com>
2019-09-13 10:47:35 +0200

    add working UISlider, time observer, and player time display; update some comments

--- Ovid/ViewController.swift
+++ Ovid/ViewController.swift
@@ -14,6 +14,30 @@ import AVKit
 import AVFoundation
 import MessageUI
 
+
+extension CMTime {
+    var asDouble: Double {
+        get {
+            return Double(self.value) / Double(self.timescale)
+        }
+    }
+    var asFloat: Float {
+        get {
+            return Float(self.value) / Float(self.timescale)
+        }
+    }
+}
+
+extension CMTime: CustomStringConvertible {
+    public var description: String {
+        get {
+            let seconds = Int(round(self.asDouble))
+            return String(format: "%02d:%02d", seconds / 60, seconds % 60)
+        }
+    }
+}
+
+
 class ViewController: UIViewController, ARSCNViewDelegate, MFMailComposeViewControllerDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
@@ -25,9 +49,18 @@ class ViewController: UIViewController, ARSCNViewDelegate, MFMailComposeViewCont
     
     var player: AVQueuePlayer!
     
+    var playerItem: AVPlayerItem!
+    
     var isPlaying: Bool = true
+    
+    var duration: CMTime = CMTime(seconds: 0, preferredTimescale: 100)
+    
+    
+    @IBOutlet weak var slider: UISlider!
+   
+    @IBOutlet weak var playerCurrentTime: UILabel!
 
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,7 +70,32 @@ class ViewController: UIViewController, ARSCNViewDelegate, MFMailComposeViewCont
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        
+        // Initialize AVQueuePlayer
+        let url = Bundle.main.url(forResource: "HubSpot-AboutUs.mp4", withExtension: nil)
+        
+        playerItem = AVPlayerItem(url: url!)
+        
+        player = AVQueuePlayer(playerItem: playerItem)
+        
+        
+        // Add time observer
+        player!.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 100), queue: DispatchQueue.main, using: { (cmtime) in
+//            print(cmtime)
+            
+            self.slider.value = Float(CMTimeGetSeconds(cmtime)) / Float(CMTimeGetSeconds(self.duration))
+            print(self.slider.value)
+            
+            
+//            self.slider.setMinimumTrackImage(<#T##image: UIImage?##UIImage?#>, for: <#T##UIControl.State#>)
+//            self.slider.setMaximumTrackImage(<#T##image: UIImage?##UIImage?#>, for: <#T##UIControl.State#>)
+//            self.slider.minimumValue = / self.duration
+            
+            self.playerCurrentTime.text = "\(cmtime.description) / \(self.duration)"
+            })
+        
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -53,20 +111,33 @@ class ViewController: UIViewController, ARSCNViewDelegate, MFMailComposeViewCont
             
             // Our configuration currently tracks only one image
             configuration.maximumNumberOfTrackedImages = 1
-            
         }
-        
-//        let url = URL(string: "https://hubspot.hubs.vidyard.com/watch/Jgw4cuRZkXyuxZ3hQnoMAv?")
-        
-        // Initialize AVQueuePlayer
-        let url = Bundle.main.url(forResource: "HubSpot-AboutUs.mp4", withExtension: nil)
-        
-        let playerItem: AVPlayerItem = AVPlayerItem(url: url!)
-        
-        player = AVQueuePlayer(playerItem: playerItem)
         
         // Run the view's session
         sceneView.session.run(configuration)
+        
+        //        let url = URL(string: "https://hubspot.hubs.vidyard.com/watch/Jgw4cuRZkXyuxZ3hQnoMAv?")
+        
+    }
+    
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+        
+        let seekTime = CMTime(seconds: Double(slider.value) * self.duration.asDouble, preferredTimescale: 100)
+        self.seekToTime(seekTime)
+
+    }
+    
+
+    private func seekToTime(_ seekTime: CMTime) {
+        self.player.seek(to: seekTime)
+    }
+    
+    
+    private func setTextLabel(cmtime: CMTime) -> UILabel {
+        let label = UILabel()
+        label.text = cmtime.description
+        return label
     }
     
     
@@ -78,13 +149,14 @@ class ViewController: UIViewController, ARSCNViewDelegate, MFMailComposeViewCont
         
     }
     
+    
     // MARK: - ARSCNViewDelegate
     
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         // the anchor is the image that it found or recognized
         
         
-        // imageAnchor is the harry potter image on the physical newspaper
+        // imageAnchor is the business card or other collateral
         if let imageAnchor = anchor as? ARImageAnchor {
         
 //            let videoURL = Bundle.main.url(forResource: "HubSpot-AboutUs.mp4", withExtension: nil)
@@ -94,9 +166,12 @@ class ViewController: UIViewController, ARSCNViewDelegate, MFMailComposeViewCont
             
             let videoNode = SKVideoNode(avPlayer: player)
 
+            duration = self.player!.currentItem!.asset.duration
+//            print("DURATION: \(duration)")
+            
             player.play()
             isPlaying = true
-            
+
             
             // the videoNode is a SpriteKit video node and we need to add that to a SceneKit element (SCNPlane below) so we can place the SceneKit element into our Scene View session. To do that, we need to create a new scene:
             // the CGSize is an estimation (480p x 360p in resolution)
@@ -131,6 +206,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, MFMailComposeViewCont
         
     }
 
+    // Hold pause state even if anchorImage is removed from and then returned to SCNView
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         if !isPlaying {
             player.pause()
